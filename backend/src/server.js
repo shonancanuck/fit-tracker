02b4e10c2e515694a8fc19b853773fee2f8fc360
shonancanuck <shonanxspark@gmail.com>
2023-07-15 16:56:16 +0900

    feat: Edit server to allow user route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,22 @@
 const express = require("express");
-const app = express();
+const path = require("path");
+
 const cors = require("cors");
 const allowedOrigins = ["http://localhost:3000"];
 const allowedMethods = ["GET", "POST"];
 const port = process.env.PORT || 3001;
 
+// Routes
 const exercise = require("./controller/exercise");
-// const user = require("./controller/user");
+const user = require("./controller/user");
 const userHistory = require("./controller/history");
 
+const app = express();
 app.use(express.json());
 app.use(cors({ origin: allowedOrigins, methods: allowedMethods }));
 app.use("/", express.static("public"));
 app.use("/exercise", exercise);
-// app.use("/user", user);
+app.use("/user", user);
 app.use("/user/history", userHistory);
 
 app.get("/", (req, res) => res.send("Hello!"));
