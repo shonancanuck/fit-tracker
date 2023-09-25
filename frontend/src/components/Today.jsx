7a2a87842ad1ff31efcf911ee74e6d2afe30110a
shonancanuck <shonanxspark@gmail.com>
2023-09-25 17:16:11 +0900

    feat: implement selection list for todays workout

--- frontend/src/components/Today.jsx
+++ frontend/src/components/Today.jsx
@@ -1,3 +1,57 @@
+import React, { useState, useEffect, useContext } from "react";
+import UserContext from "../contexts/UserContext";
+
 export default function Today() {
-  return <h3>Today's workout</h3>;
+  const [exerciseList, setExerciseList] = useState([]);
+  const { userId } = useContext(UserContext);
+  const [selectedExercise, setSelectedExercise] = useState(null);
+
+  useEffect(() => {
+    getExercises();
+  }, []);
+
+  useEffect(() => {
+    console.log(selectedExercise);
+  }, [selectedExercise]);
+
+  const getExercises = async () => {
+    try {
+      const exData = await fetch("http://localhost:3001/exercise");
+      const exList = await exData.json();
+      setExerciseList(exList);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleSelection = (e) => {
+    const selection = exerciseList.find(
+      (ex) => ex["exercise_name"] === e.target.className
+    );
+    setSelectedExercise(selection);
+  };
+
+  const handleUnselection = () => {
+    setSelectedExercise(null);
+  };
+
+  return (
+    <>
+      <h3>Today's workout</h3>
+      <main>
+        <p onClick={handleUnselection}>back</p>
+        <ul>
+          {exerciseList.map((ex, index) => (
+            <li
+              key={ex["exercise_name"]}
+              className={ex["exercise_name"]}
+              onClick={handleSelection}
+            >
+              {ex["exercise_name"]}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
 }
