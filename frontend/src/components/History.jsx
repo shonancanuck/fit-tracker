88618a55ab4fddfb318aa11d57ae468ee36d42d8
shonancanuck <shonanxspark@gmail.com>
2023-09-25 15:22:23 +0900

    fix: implement exercise selection in history component properly

--- frontend/src/components/History.jsx
+++ frontend/src/components/History.jsx
@@ -5,62 +5,77 @@ export default function History() {
   const [exerciseList, setExerciseList] = useState([]);
   const { userId } = useContext(UserContext);
   const [selectedHistory, setSelectedHistory] = useState(null);
-  const [extendedHistory, setExtendedHistory] = useState(null);
+  const [displayHistory, setDisplayHistory] = useState(null);
 
   useEffect(() => {
     getExercises();
   }, []);
 
-  useEffect(() => {});
+  useEffect(() => {
+    getHistory();
+    console.log(selectedHistory);
+  }, [selectedHistory]);
 
   const getExercises = async () => {
     try {
       const exData = await fetch("http://localhost:3001/exercise");
       const exList = await exData.json();
       setExerciseList(exList);
-      console.log(exerciseList);
     } catch (err) {
       console.error(err);
     }
   };
 
-  const selectHistory = (e) => {
-    if (selectedHistory) {
-      setSelectedHistory(null);
-    } else {
-      const selection = exerciseList.find(
-        (ex) => ex["exercise_name"] === e.target.className
-      );
-      setSelectedHistory(selection);
-    }
-    console.log(selectedHistory);
+  const handleSelection = (e) => {
+    const selection = exerciseList.find(
+      (ex) => ex["exercise_name"] === e.target.className
+    );
+    setSelectedHistory(selection);
+  };
+
+  const handleUnselection = () => {
+    setSelectedHistory(null);
   };
 
   const getHistory = async () => {
-    try {
-      const historyData =
-        await fetch(`http://localhost:3001/history/extended/${userId}/
-      ${selectedHistory["exercise_id"]}`);
-      const exHistory = await historyData.json();
-      setExtendedHistory(exHistory);
-      console.log(extendedHistory);
-    } catch (err) {
-      console.error(err);
+    if (selectedHistory) {
+      try {
+        const historyData =
+          await fetch(`http://localhost:3001/history/extended/user/${userId}/exercise/
+        ${selectedHistory["exercise_id"]}`);
+        const exHistory = await historyData.json();
+        setDisplayHistory(exHistory);
+        console.log(displayHistory);
+      } catch (err) {
+        console.error(err);
+      }
     }
   };
-  return (
-    <>
-      <ul>
-        {exerciseList.map((ex, index) => (
-          <li
-            key={ex["exercise_name"]}
-            className={ex["exercise_name"]}
-            onClick={selectHistory}
-          >
-            {ex["exercise_name"]}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+
+  if (!selectedHistory) {
+    return (
+      <>
+        <h3>Select an exercise to show your progress</h3>
+        <main>
+          <ul>
+            {exerciseList.map((ex, index) => (
+              <li
+                key={ex["exercise_name"]}
+                className={ex["exercise_name"]}
+                onClick={handleSelection}
+              >
+                {ex["exercise_name"]}
+              </li>
+            ))}
+          </ul>
+        </main>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p onClick={handleUnselection}>back</p>
+      </>
+    );
+  }
 }
